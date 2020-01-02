96aa8bcb5a26c53fb5ebf4b99bff07d7956f6b1e
SohelRaja <rohan.nsec@gmail.com>
2020-01-02 14:48:52 +0530

    input with yargs instead of process and all fuctionality

--- notes.js
+++ notes.js
@@ -1,5 +1,21 @@
 console.log('Notes App started');
 
-module.exports.addNote = () => {
-    return "new note";
+var addNote = (title, body) => {
+    console.log('Adding a new note',title,body);
+};
+var getAll = () => {
+    console.log('Listing all notes');
+};
+var getNote = (title) => {
+    console.log('Reading note',title);
+};
+var removeNote = (title) => {
+    console.log('Removing note',title);
+};
+
+module.exports = {
+    addNote: addNote, 
+    getAll: getAll,
+    getNote: getNote,
+    removeNote: removeNote
 };
